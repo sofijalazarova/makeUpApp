62a48c22390733b5c709dc9143b1913f586046a9
sofijalazarova <94320367+sofijalazarova@users.noreply.github.com>
2024-06-26 15:25:23 +0200

    feat: react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Outlet, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Products from './components/Products';
 import HomePage from './pages/HomePage';
@@ -9,19 +9,25 @@ import { HashRouter as Router } from "react-router-dom";
 const App = () => {
   
   return (         
-    <Router>
-      <div className="relative z-0">
-        <div>
-          <Navbar/>
-        </div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/products" element={<Products />} />      
-          <Route path="/productDetails/:id" element={<ProductDetails />} />      
-          <Route path="/heroProduct" element={<ExclusiveProduct />} />
-        </Routes>
+    // <Router>
+    //   <div className="relative z-0">
+    //     <div>
+    //       <Navbar/>
+    //     </div>
+    //     <Routes>
+    //       <Route path="/" element={<HomePage />} />
+    //       <Route path="/products" element={<Products />} />      
+    //       <Route path="/productDetails/:id" element={<ProductDetails />} />      
+    //       <Route path="/heroProduct" element={<ExclusiveProduct />} />
+    //     </Routes>
+    //   </div>
+    // </Router>
+    <>
+      <div>
+        <Navbar/>
+        <Outlet />
       </div>
-    </Router>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: "/makeUpApp/",
+    element: <App/>,
+    children: [
+      {
+        path: "/makeUpApp/",
+        element: <HomePage/>,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
